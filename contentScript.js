e62fe75daa1c2fa107cf3f60a64784565a717de0
Dongmin Kim <d-kim@isi.imi.i.u-tokyo.ac.jp>
2024-01-01 21:08:43 +0900

    Change to firefox extension

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,20 @@
-chrome.storage.sync.get(['isEnabled', 'blog', 'cafe', 'map'], function(data) {
-    var currentURL = window.location.href;
-    if (data.isEnabled && data.blog && currentURL.includes("m.blog.naver")) {
-        var newURL = currentURL;
-        if (newURL.includes("PostView.naver")) {
-            newURL = newURL.replace("PostView.naver", "PostView.nhn");
-        }
-        newURL = newURL.replace("m.blog.naver", "blog.naver");
-        window.location.replace(newURL);
+console.log("fuck");
+
+var currentURL = window.location.href;
+if (currentURL.includes("m.blog.naver")) {
+    var newURL = currentURL;
+    if (newURL.includes("PostView.naver")) {
+        newURL = newURL.replace("PostView.naver", "PostView.nhn");
     }
-    if (data.isEnabled && data.cafe && currentURL.includes("m.cafe.naver")) {
-        var newURL = currentURL.replace("m.cafe.naver", "cafe.naver");
-        window.location.replace(newURL);
-    }
-    if (data.isEnabled && data.map && currentURL.includes("m.map.naver")) {
-        var newURL = currentURL.replace("m.map.naver", "map.naver");
-        window.location.replace(newURL);
-    }
-});
+    newURL = newURL.replace("m.blog.naver", "blog.naver");
+    window.location.replace(newURL);
+}
+if (currentURL.includes("m.cafe.naver")) {
+    var newURL = currentURL.replace("m.cafe.naver", "cafe.naver");
+    window.location.replace(newURL);
+}
+if (currentURL.includes("m.map.naver")) {
+    var newURL = currentURL.replace("m.map.naver", "map.naver");
+    window.location.replace(newURL);
+}
+
